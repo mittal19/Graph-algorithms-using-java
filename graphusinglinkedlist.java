552a2a12c64872fe4ac3320ea7eedba60c29846d
Priyanshu Mittal <mittalpriyanshu19@gmail.com>
2019-01-10 22:26:19 +0530

    Add files via upload

---
+++ graphusinglinkedlist.java
@@ -0,0 +1,44 @@
+import java.util.LinkedList;
+import java.util.*;
+class graphusinglinkedlist
+{ 
+    int V;
+    LinkedList<Integer> vertices[];
+    graphusinglinkedlist(int v)
+    { 
+        V=v;
+        vertices=new LinkedList[V];
+      for(int i=0;i<V;i++)
+      { 
+          vertices[i]=new LinkedList<>();
+      }
+    }
+    void add(int src,int dest)
+    { 
+        vertices[src].addFirst(dest);
+        vertices[dest].addFirst(src);
+    }
+    void display()
+    {
+        for(int i=0;i<V;i++)
+        { 
+            for(Integer p:vertices[i])
+              System.out.print("->"+p);
+            System.out.println();
+        }
+    }
+    public static void main()
+    {
+        Scanner in=new Scanner(System.in);
+        int v=in.nextInt();
+        graphusinglinkedlist g=new graphusinglinkedlist(v);
+        int n=in.nextInt();
+        for(int i=1;i<=n;i++)
+        { 
+            g.add(in.nextInt(),in.nextInt());
+        }
+        g.display();
+    }
+}
+        
+        
